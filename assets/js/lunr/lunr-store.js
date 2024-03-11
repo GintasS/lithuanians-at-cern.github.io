4c6ddfd244cb55bb29ca435813c2bb640f1d5a3c
GintasS <GintasS@users.noreply.github.com>
2024-03-11 16:47:04 +0000

    jekyll build from Action 3a10daef6238cef15bef7538348e8b80de39b32a

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,131 @@
+var store = [{
+        "title": "Alumin 1",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/contacts/alumni/alumni1/",
+        "teaser":"https://mmistakes.github.io/minimal-mistakes/assets/images/foo-bar-identity-th.jpg"},{
+        "title": "Alumin 2",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/contacts/alumni/alumni2/",
+        "teaser":"https://mmistakes.github.io/minimal-mistakes/assets/images/foo-bar-identity-th.jpg"},{
+        "title": "CERN School of Computing",
+        "excerpt":"CERN School of Computing organizuoja 3 mokymosi konferencijas per metus. Daugiau informacijos jų puslapyje. The CERN School of Computing promotes advanced learning and knowledge exchange in scientific computing among young scientists and engineers involved in particle physics or other sciences. It is made up of 3 separate schools per year....","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/conferences/2018-03-15/",
+        "teaser":null},{
+        "title": "ESC18 - EFFICIENT SCIENTIFIC COMPUTING",
+        "excerpt":"will be held in Bertinoro (Forlì Cesena, Italy) from the 22nd to the 27th October 2018. The aim of the ESC school is to offer the participants the opportunity to improve their computing competences, learning from qualified and experiences scientists how to best exploit modern hardware and software technologies in...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/conferences/2018-07-19/",
+        "teaser":null},{
+        "title": "Aleksas Mazeliauskas",
+        "excerpt":"Sunkiųjų jonų fizikas teoretikas (CERN)  Studijos ir mokslinė veikla B.A., MMath, 2012, Kembridžo universitetas;PhD., 2017, Stony Brook universitetas;Postdokas, 2017-2019, Haidelbergo universitetas;Fellow, 2019-dabar, CERN; Dominančios sritys   Sunkiųjų jonų energijų fizika: kvarkų–gliuonų plazmos savybėsKontaktai   Asmeninis puslapis","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/contacts/people/Aleksas_Mazeliauskas/",
+        "teaser":"https://mmistakes.github.io/minimal-mistakes/assets/images/kolektyvas/Aleksas_Mazeliauskas_preview.jpg"},{
+        "title": "Aurelijus Rinkevičius",
+        "excerpt":"Didelių energijų fizikas eksperimentatorius (Vilniaus universitetas) Mokslinė-pedagoginė istorija B.A., 2010, Vilniaus universitetas;PhD., 2014, Floridos universitetas (University of Florida);Postdokas, 2015-2019, Kornelio universitetas (Cornell University);Vartotojas, 2009-dabar, CERN;Vyriausiasis mokslo darbuotojas, 2019-dabar, Vilniaus universitetas;Centro vadovas, 2019-dabar, Bendradarbiavimo su CERN centras, Vilniaus universitetas. Kita veikla Vartotojų komiteto atstovas, 2018-dabar, CERN;Mokslinis patarėjas tarybai, 2019-dabar, CERN;Lietuvos kontaktinis...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/contacts/people/Aurelijus_Rinkevicius/",
+        "teaser":"https://mmistakes.github.io/minimal-mistakes/assets/images/kolektyvas/Aurelijus_Rinkevicius_preview.webp"},{
+        "title": "Mantas Stankevičius",
+        "excerpt":"Programuotojas Bendradarbiavimas su CERN prasidėjo 2010 m. Šiuo metu dirbu CMS eksperimento DAQ grupės OMS (Online Monitoring System) projekte. Renkame duomenis apie CMS detektoriaus darbą bei apdorojame (Data Warehouse). Duomenys vartotojams pateikiami per naršyklę arba API. Organizuoju CERN lietuvių susirinkimus. Išsilavinimas B.S. 2010, Vilniaus Universitetas, Informatika M.S. 2012, Vilniaus Universitetas,...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/contacts/people/Mantas_Stankevicius/",
+        "teaser":"https://mmistakes.github.io/minimal-mistakes/assets/images/kolektyvas/Mantas_Stankevicius_preview.jpg"},{
+        "title": "Template",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/contacts/people/template/",
+        "teaser":null},{
+        "title": "CERN vasaros stažuotė bakalauro ir magistro studentams",
+        "excerpt":"CERN kviečia trečio kurso ir vyresnius informacinių technologijų studentus teikti paraiškasdevynių savaičių apmokymų ir darbo programai 2020 vasarą. Į programą įtraukti konkretūs darbo projektai, specializuotos paskaitos bei CERN eksperimentų lankymas. Paraiškas (CV, studijų išrašą ir rekomendacijas) reikia pateikti iki 2020 m. sausio 31 d. Šaukimo aprašymą rasite čia ","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/interships/2019-12-01/",
+        "teaser":null},{
+        "title": "CERN vasaros stažuotė bakalauro ir magistro studentams",
+        "excerpt":"CERN kviečia trečio kurso ir vyresnius tiksliųjų mokslų (fizikos, inžinerijos, kompiuterijos, matematikos) studentus teikti paraiškaisvasaros apmokymų ir darbo programai 2020 vasarą. Į programą įtraukti konkretūs darbo projektai su CERN mokslininkais ir specialistais, specializuotos paskaitos bei CERN eksperimentų lankymas. Paraiškas (CV, studijų išrašą ir rekomndacijas) reikia pateikti iki 2020 m. sausio...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/interships/2019-12-02/",
+        "teaser":null},{
+        "title": "M&O Cat.A open position in the CMS PPD Group",
+        "excerpt":"We have an opening for a M&amp;O Cat. A position in the CMS PPD group that we aim to fill in by 1 March 2020 for Operation of the Alignment and Calibration infrastructure for the PPD project of the CMS experiment. Please see the job description below. The interested candidates...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/jobs/2019-12-28-skelbimas/",
+        "teaser":null},{
+        "title": "Postdoc position in theoretical physics in Vilnius",
+        "excerpt":"Institute of Theoretical Physics and Astronomy of Vilnius University, Lithuania, is offering a two year postdoc position. Description on Inspire ","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/jobs/2020-11-15-skelbimas/",
+        "teaser":null},{
+        "title": "Postdoc position in experimental high energy physics",
+        "excerpt":"The Experimental High Energy Physics group at Vilnius University has openings for postdoctoral research associates in CMS detector upgrade. Description on Inspire ","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/jobs/2020-11-15-skelbimas2/",
+        "teaser":null},{
+        "title": "Pranesimas 1",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/messages/2018-03-20/",
+        "teaser":null},{
+        "title": "Pranesimas 2",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/messages/2018-04-01/",
+        "teaser":null},{
+        "title": "Pranesimas 3",
+        "excerpt":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint...","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/messages/2018-04-10/",
+        "teaser":null},{
+        "title": "Pasirašyta CERN asocijuotosios narystės sutartis",
+        "excerpt":"Birželio 27d. Vilniuje CERN Generalinė direktorė Fabiola Gianotti ir Lietuvos užsienio reikalų ministras Linas Linkevičius kartu su Lietuvos prezidente Dalia Grybauskaite pasirašė sutartį dėl Lietuvos asocijuotos narystės CERN. Sutartis įsigalios, kai ją ratifikuos Lietuvos Respublikos Seimas.  CERN pranešimas anglų kalba ","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/pasira%C5%A1ytas-asocijuotos-naryst%C4%97s-sutartis/",
+        "teaser":null},{
+        "title": "Ratifikuota CERN sutartis",
+        "excerpt":"Gruodžio 7d. Lietuvos Seimas vienbalisiais priėmė įstatymą dėl DĖL LIETUVOS RESPUBLIKOS IR EUROPOS BRANDUOLINIŲ MOKSLINIŲ TYRIMŲ ORGANIZACIJOS (CERN) SUSITARIMO DĖL CERN ASOCIJUOTOJO NARIO STATUSO SUTEIKIMO RATIFIKAVIMO. Oficialus teisės akto tekstas ","categories": [],
+        "tags": [],
+        "url": "https://mmistakes.github.io/minimal-mistakes/ratifikuota-CERN-naryst%C4%97s-sutartis/",
+        "teaser":null},{
+        "title": "Įsigaliojo Lietuvos asocijuotos CERN narystės sutartis",
+        "excerpt":"Sauso 8d. Lietuvos ambasadorius prie Jungtinių Tautų biuro Andrius Krivys įteikė oficialią Lietuvos nota dėl Lietuvos asocijuotės narystės CERN ratifikavimo ir Lietuva oficialiai tapo CERN nare. CERN pranešimas anglų kalba. ","categories": [],
+        "tags": ["naujienos"],
+        "url": "https://mmistakes.github.io/minimal-mistakes/Lietuva-tampa-CERN-nare/",
+        "teaser":null},{
+        "title": "Pirmas Lietuvos posėdis CERN taryboje",
+        "excerpt":"Kovo 12–15 dienomis Ženevoje posėdžiauja Europos branduolinių mokslinių tyrimų organizacijos (CERN) Taryba, kurios darbe pirmą kartą dalyvaus ir Lietuvos delegacija. Lietuvos mokslų akademijos, Lietuvos pramoninkų konfederacijos, Vilniaus universiteto, Švietimo ir mokslo ministerijos ir Užsienio reikalų ministerijos atstovai dalyvaus posėdžiuose, kuriuose bus aptariami elementariųjų dalelių fizikos strategijos klausimai, prekių ir paslaugų...","categories": [],
+        "tags": ["naujienos"],
+        "url": "https://mmistakes.github.io/minimal-mistakes/Lietuva-CERN-taryboje/",
+        "teaser":null},{
+        "title": "Mokslinio meistriškumo kursai moksleiviams",
+        "excerpt":"Kovo 23 d. Vilniaus fizikos fakulteto Branduolių ir elementariųjų dalelių fizikos centras rengia elementariųjų dalelių meistriškumo pamokas moksleiviams. Šio tarptautinio renginio metu jaunuoliai galės susipažinti su realių CERN duomenų apdorojimu ir patys galės išmėginti savo jėgas. Plačiau apie renginį. ","categories": [],
+        "tags": ["renginai"],
+        "url": "https://mmistakes.github.io/minimal-mistakes/meistri%C5%A1kumo-kursai/",
+        "teaser":null},{
+        "title": "Antrasis CERN Baltijos susirinkimas",
+        "excerpt":"Šią savaitę įvyko CERN Baltijos grupės (CBG) susirinkimas, kurio metu aštuonios Baltijos aukštosios mokyklos pasirašė tarpusavio bendradarbiavimo memorandumą (Memorandum of Understanding).Oficialiai startuoja CERN Baltijos grupė. Įsteigti trys CBG pogrupiai: CMS, studijų ir santykiams su industrija.  ","categories": [],
+        "tags": ["CERN","Baltic","Group"],
+        "url": "https://mmistakes.github.io/minimal-mistakes/CERN-Baltic-mtg2/",
+        "teaser":null},{
+        "title": "Tyrėjų naktis CERNe 2018",
+        "excerpt":"Tai antrasis toks renginys, kuriame lietuvos moksleiviai ir studentai virtualiubūdu aplankė CMS eksperimentą CERNe tyrėjų nakties metu! CERNo lietuviai buvo gidais ir renginį vedė lietuvių kalba. Pirmasis toks renginys tyrėjams įvyko 2017-ais metais, o prieš tai buvo dar du skirti specialiai mokykloms. Jei domina, ragink savo savo švietimo sklaidos centrą...","categories": [],
+        "tags": ["tyrėjų","naktis","CMS","virtualus","vizitas","lietuviai"],
+        "url": "https://mmistakes.github.io/minimal-mistakes/CERN-tyreju-naktis/",
+        "teaser":null},{
+        "title": "Šveicarijos profesionalų klubas (GLL diaspora) susirinko CERNe",
+        "excerpt":"Šiemet startavęs Lietuvių profesionalų klubas Šveicarijoje (kaip GLL diaspora)pirmą kartą susirinko CERNe. Tiek Lietuviams CERNe, tiek lietuvos profesionalamsŠveicarijoje buvo įdomu geriau susipažinti, pakalbėti apie mokslą, politiką,Lietuvą ir kitas aktualias temas. Renginyje dalyvavo ambasadorius prie JungtiniųTautų Ženevoje Andrius Krivas ir konsulas Šveicarijai Eduardas Petkevičius. Taippat renginį aplankė profesionalų klubo nariai iš...","categories": [],
+        "tags": ["CERN","GLL","Šveicarija","profesionalai","klubas"],
+        "url": "https://mmistakes.github.io/minimal-mistakes/CERN-GLL-profesionalai/",
+        "teaser":null},{
+        "title": "CERN taryba koreguoja LHC paleidimo tvarkaraštį",
+        "excerpt":"2019 gruodžio 9–13 dienomis Ženevoje posėdžiavo Europos branduolinių mokslinių tyrimų organizacijos (CERN) Taryba, kurioje Lietuvą atstovaujaBendradarbiavimo su CERN centro vadovas Aurelijus Rinkevičius. Taryba nusprendė porai mėnesių atidėti trečiajį LHC veiklos periodo pradžią iki 2021 m. gegužės, bei pratęsti jo veiklos trukmę metams. Atitinkamai didelio intensyvumo LHC (HL-LHC) pradės savo veiklą...","categories": [],
+        "tags": ["naujienos"],
+        "url": "https://mmistakes.github.io/minimal-mistakes/CERN-tarybos-savait%C4%97/",
+        "teaser":null},{
+        "title": "Pirmasis Lietuvos dalelių fizikos susitikimas",
+        "excerpt":"2021 m. kovo 9 d. dieną įvyko pirmasis Lietuvos dalelių fizikos susitikimas (angl. Lithuanian Particle Physics Meeting – LPPM 2021). Jame susirinko virš 30 lietuvių mokslininkų iš aštuonių pasaulio šalių. Septyniolika pranešėjų pristatė savo karjeros kelius ir dabartines tyrimų sritis. Lietuviai fizikai eksperimentatoriai dalijosi savo darbo patirtimi prie tokių tarptautinių...","categories": [],
+        "tags": ["naujienos"],
+        "url": "https://mmistakes.github.io/minimal-mistakes/LPPM2021/",
+        "teaser":null}]
